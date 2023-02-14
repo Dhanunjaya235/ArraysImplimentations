1f87378a2dbe38e5425589ee0d572669f843f8c5
Dhanunjaya235 <120103263+Dhanunjaya235@users.noreply.github.com>
2023-02-14 19:04:19 +0530

    Create Class3.cs

---
+++ ArraysImplimentations/Class3.cs
@@ -0,0 +1,10 @@
+namespace ArraysImplimentations
+{
+    internal class ArrayMethods
+    {
+        public int Method(int[] array,int index)
+        {
+            return array[index];
+        }
+    }
+}
