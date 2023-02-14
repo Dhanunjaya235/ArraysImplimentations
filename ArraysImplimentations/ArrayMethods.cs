e07f8735b9a730f7759f083651b240cf5d56afa0
Dhanunjaya235 <dhanunjaya.andavarapu@cognine.com>
2023-02-14 19:01:48 +0530

    New Class Created

---
+++ ArraysImplimentations/ArrayMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysImplimentations
+{
+    internal class ArrayMethods
+    {
+        public int Method(int[] array,int index)
+        {
+            return array[index];
+        }
+    }
+}
